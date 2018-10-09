140db851f70bca05bcce29320503366490618e7e
blackopss <36933564+blackopss@users.noreply.github.com>
2018-10-09 23:30:02 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,16 +1,15 @@
 
 const Discord = require('discord.js');
- const RichEmbed = require('discord.js');
 const client = new Discord.Client();
 const prefix = '.'
 
-client.on('ready', () => {
+client.on('message', message => {
   console.log(`Emoko maid ready to pleasure you daddy ~`);
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+client.on('message', message => {
+  if (message.content === 'ping') {
+    message.reply('Pong!');
   }
 });
 
