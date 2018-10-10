f22a24dfdee3ac9f06b4f0eb79963100952739f7
RESKORESKO65 <42848880+RESKORESKO65@users.noreply.github.com>
2018-10-10 23:02:10 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,13 @@
-
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '!'
-const adminprefix = "!";
-const developers = ['482639461306335232'];
+const adminprefix = "-";
+const developers = ['474305973352398848'];
+
+console.log("Paaaaartly");
 
 client.on('ready', () => {
- console.log("hi"); 
-console.log("bye");
-});
-
+    console.log(`Logged as ${client.user.tag}`)
+})
 
 
 client.on('message', message => {
@@ -18,24 +16,31 @@ client.on('message', message => {
       
   if (message.content.startsWith(adminprefix + 'ply')) {
     client.user.setGame(argresult);
+      message.channel.send(`**تم تغيير البلاينق الى   ${argresult}**`)
   } else 
      if (message.content === (adminprefix + "leave")) {
     message.guild.leave();        
   } else  
   if (message.content.startsWith(adminprefix + 'wt')) {
   client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.send(`**تَم تغيير الواتشينق الى   ${argresult}**`)
   } else 
   if (message.content.startsWith(adminprefix + 'ls')) {
   client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send(`**تَم تغيير الليسينينق الى   ${argresult}**`)
   } else
   if (message.content.startsWith(adminprefix + 'st')) {
     client.user.setGame(argresult, "https://www.twitch.tv/paaaaaartly");
+      message.channel.send(`تم تغييرك حالتك بالتويتش الى   **${argresult}**`)
   }
   if (message.content.startsWith(adminprefix + 'sn')) {
   client.user.setUsername(argresult).then
+      message.channel.send(`جاري تغيير الأسم لـ ..**${argresult}** `)
 } else
 if (message.content.startsWith(adminprefix + 'sa')) {
   client.user.setAvatar(argresult);
+    message.channel.send(`**جاري تغيير الأفتار... :** `);
 }
 });
-client.login(process.env.BOT_TOKEN); 
+
+client.login("NDgyNjM5NDYxMzA2MzM1MjMy.Dp_qxw.rMgFy_1cC4e3wc9KoW34YfQGyp0") 
