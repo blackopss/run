c4092c1c6c296e55d0779cd7ffc0e4bff8b1e0b2
RESKORESKO65 <42848880+RESKORESKO65@users.noreply.github.com>
2018-10-12 14:36:57 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const adminprefix = "!";
-const developers = ['482639461306335232'];
+const developers = ['498517748918386690'];
 
 console.log("Paaaaartly");
 
@@ -34,4 +34,4 @@ client.on('message', message => {
 });
 
 
-client.login("NDk4NTE3NzQ4OTE4Mzg2Njkw.DqIF4A.4msu-cg7dYBwogcO9rlS0bqLQxY") 
+client.login("NDk4NTE3NzQ4OTE4Mzg2Njkw.DqIF4A.4msu-cg7dYBwogcO9rlS0bqLQxY")
