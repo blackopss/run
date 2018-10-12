5a8b248bf09f1c71fe24e59cb657c24674cf312a
RESKORESKO65 <42848880+RESKORESKO65@users.noreply.github.com>
2018-10-12 14:34:46 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,46 +1,37 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const adminprefix = "!";
-const developers = [`500260185122668564`];
+const developers = ['482639461306335232'];
 
 console.log("Paaaaartly");
 
-client.on('ready', (!) => {
+client.on('ready', () => {
     console.log(`Logged as ${client.user.tag}`)
 })
 
 
 client.on('message', message => {
-    var argresult = message.content.split(` `).slice(1).join(' ');
-      if (!developers.includes(message.author.id)) return;
-      
-  if (message.content.startsWith(adminprefix + 'ply')) {
-    client.user.setGame(argresult);
-      message.channel.send(`**تم تغيير البلاينق الى   ${argresult}**`)
-  } else 
-     if (message.content === (adminprefix + "leave")) {
-    message.guild.leave();        
-  } else  
-  if (message.content.startsWith(adminprefix + 'wt')) {
-  client.user.setActivity(argresult, {type:'WATCHING'});
-      message.channel.send(`**تَم تغيير الواتشينق الى   ${argresult}**`)
-  } else 
-  if (message.content.startsWith(adminprefix + 'ls')) {
-  client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.send(`**تَم تغيير الليسينينق الى   ${argresult}**`)
-  } else
-  if (message.content.startsWith(adminprefix + 'st')) {
-    client.user.setGame(argresult, "https://www.twitch.tv/paaaaaartly");
-      message.channel.send(`تم تغييرك حالتك بالتويتش الى   **${argresult}**`)
-  }
-  if (message.content.startsWith(adminprefix + 'sn')) {
-  client.user.setUsername(argresult).then
-      message.channel.send(`جاري تغيير الأسم لـ ..**${argresult}** `)
-} else
-if (message.content.startsWith(adminprefix + 'sa')) {
-  client.user.setAvatar(argresult);
-    message.channel.send(`**جاري تغيير الأفتار... :** `);
-}
+  if (!message.content.startsWith(prefix)) return;
+  var args = message.content.split(' ').slice(1);
+  var argresult = args.join(' ');
+  if (message.author.id !== "482639461306335232") return;
+
+  
+  if (message.content.startsWith(prefix + 'setwatching')) {
+  client.user.setActivity(argresult, {type: 'WATCHING'})
+     console.log('watching be' + argresult);
+    message.channel.sendMessage(`Watch Now: **${argresult}**`)
+} 
+
+ 
+  if (message.content.startsWith(prefix + 'setlis')) {
+  client.user.setActivity(argresult, {type: 'LISTENING'})
+     console.log('LISTENING be' + argresult);
+    message.channel.sendMessage(`LISTENING Now: **${argresult}**`)
+} 
+
+
 });
 
-client.login("NDk4NTE3NzQ4OTE4Mzg2Njkw.DqIF4A.4msu-cg7dYBwogcO9rlS0bqLQxY")
+
+client.login("NDk4NTE3NzQ4OTE4Mzg2Njkw.DqIF4A.4msu-cg7dYBwogcO9rlS0bqLQxY") 
