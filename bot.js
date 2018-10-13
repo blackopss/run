20e5c3df57146ac5f51cf4ad9fdaf34903a936ea
RESKORESKO65 <42848880+RESKORESKO65@users.noreply.github.com>
2018-10-13 16:06:57 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,15 +9,6 @@ client.on('ready', () => {
     console.log(`Logged as ${client.user.tag}`)
 })
 
-const Discord = require('discord.js');
-const client = new Discord.Client();
-const adminprefix = "!";
-const developers = ['476410664047345694'];
-
-console.log("Paaaaartly");
-
-client.on('ready', () => {
-    console.log(`Logged as ${client.user.tag}`)
 
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
@@ -52,39 +43,4 @@ if (message.content.startsWith(adminprefix + 'sa')) {
 }
 });
 
-client.login("NDc2NDEwNjY0MDQ3MzQ1Njk0.DqN78A.uY6SeKo73p4cBAsHQv-VT-BU8ro") 
-
-client.on('message', message => {
-    var argresult = message.content.split(` `).slice(1).join(' ');
-      if (!developers.includes(message.author.id)) return;
-      
-  if (message.content.startsWith(adminprefix + 'ply')) {
-    client.user.setGame(argresult);
-      message.channel.send(`**تم تغيير البلاينق الى   ${argresult}**`)
-  } else 
-     if (message.content === (adminprefix + "leave")) {
-    message.guild.leave();        
-  } else  
-  if (message.content.startsWith(adminprefix + 'wt')) {
-  client.user.setActivity(argresult, {type:'WATCHING'});
-      message.channel.send(`**تَم تغيير الواتشينق الى   ${argresult}**`)
-  } else 
-  if (message.content.startsWith(adminprefix + 'ls')) {
-  client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.send(`**تَم تغيير الليسينينق الى   ${argresult}**`)
-  } else
-  if (message.content.startsWith(adminprefix + 'st')) {
-    client.user.setGame(argresult, "https://www.twitch.tv/paaaaaartly");
-      message.channel.send(`تم تغييرك حالتك بالتويتش الى   **${argresult}**`)
-  }
-  if (message.content.startsWith(adminprefix + 'sn')) {
-  client.user.setUsername(argresult).then
-      message.channel.send(`جاري تغيير الأسم لـ ..**${argresult}** `)
-} else
-if (message.content.startsWith(adminprefix + 'sa')) {
-  client.user.setAvatar(argresult);
-    message.channel.send(`**جاري تغيير الأفتار... :** `);
-}
-});
-
-client.login("NDgyNjM5NDYxMzA2MzM1MjMy.DqLBbw.egV6sdvqxDBblzNk4GOrlbU6sIw") 
+client.login("NDgyNjM5NDYxMzA2MzM1MjMy.DqN_Sw.rtY030CnyfZiL0AwiVpfBDxWvs4") 
